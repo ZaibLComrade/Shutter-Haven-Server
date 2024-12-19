26f8aba27fbe30b4c16bb2aed3085c409a0ee039
ZaibLComrade <kmjahanzaib@proton.me>
2024-12-20 01:34:58 +0600

    Initalized router

---
+++ src/routes/index.ts
@@ -0,0 +1,18 @@
+import {RequestHandler, Router} from "express";
+
+interface Route {
+	path: string;
+	reqHandler: RequestHandler
+}
+
+const router = Router();
+
+const routes: Route[] = [
+
+]
+
+routes.forEach(({ path, reqHandler }: Route) => {
+	router.use(path, router);
+})
+
+export default router;
