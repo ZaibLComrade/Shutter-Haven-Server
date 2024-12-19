cef89529fc09166150daf0d9a1bc20f5492a9d7b
ZaibLComrade <kmjahanzaib@proton.me>
2024-12-20 00:50:06 +0600

    Added custom ApiError class

---
+++ src/utils/ApiError.ts
@@ -0,0 +1,16 @@
+// Custom Error Class
+class ApiError extends Error {
+	constructor(
+		public statusCode: number,
+		public message: string,
+		public isOperational: boolean = true,
+		public stack: string = ""
+	) {
+		super(message);
+		if (stack === "") {
+			Error.captureStackTrace(this, this.constructor);
+		}
+	}
+}
+
+export default ApiError;
